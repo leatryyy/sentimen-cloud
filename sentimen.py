5be73e7acec64482cabd7823b7fc1e7afcd25e9c
leatryyy <157835064+leatryyy@users.noreply.github.com>
2026-07-01 10:42:43 +0700

    Create sentimen.py

---
+++ sentimen.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Sistem Analisis Sentimen Berbasis Cloud")
+st.success("Status: Infrastruktur CI/CD Berhasil Terhubung! 🚀")
+st.info("Modul Machine Learning sedang dalam tahap pengembangan.")
